d2be8cf39aeffeb6c8dbec400c283c7638d59ec1
kittasil <kittasil.silanon@gmail.com>
2020-11-09 21:23:54 +0700

    end javascript

---
+++ ๋JavaScripforReact/main.js
@@ -0,0 +1,10 @@
+import {sayHi, sayBye} from './say.js';
+
+sayHi();
+sayBye();
+
+/////////////////////////////////////////////////////
+
+// import * as say from './say.js';
+// say.sayHi();
+// say.sayBye();
---
+++ ๋JavaScripforReact/say.js
@@ -0,0 +1,10 @@
+function sayHi() {
+    console.log('sayHi');
+    
+}
+
+function sayBye(user) {
+    console.log('sayBye');
+}
+
+export {sayHi, sayBye};
